6db90632372194f19612e8a974d69601e9fbcde6
SaundersB <speakeasys@gmail.com>
2015-05-26 14:17:53 -0700

    Adding in comments for easier comprehension of directives.

--- CSE461Lab3/CSE461Lab3/Table.h
+++ CSE461Lab3/CSE461Lab3/Table.h
@@ -27,6 +27,11 @@ class Table: public Filesys
         int IndexSearch(string value);
 };
 
+
+
+
+//This constructor creates the table object. It creates the new (empty) files flatfile and indexfile in the file system on the Sdisk using diskname.
+
 Table::Table(string diskname, string flatfile, string indexfile)
 {
     fs = diskname;
@@ -36,6 +41,9 @@ Table::Table(string diskname, string flatfile, string indexfile)
     fs -> newfile(index_file);
     return 1;
 }
+
+
+//This module will read records from the input file (the raw data file described above), add the records to the flatfile and create index records consisting of the date and block number, and then add the index records to the index file. (Note that index records will have 10 bytes .. 5 bytes for the date and 5 bytes for the block number.)
 
 int Table::Build_Table
 {
@@ -62,6 +70,10 @@ int Table::Build_Table
     
     return 1;
 }
+        
+        
+        
+// This module accepts a key value, and searches the index file with a call to IndexSearch for the record where the date matches the specified value. IndexSearch returns the blocknumber of the block in the flat file where the target record is located. This block should then be read and the record displayed.
 
 int Table::Search
 {
@@ -88,6 +100,9 @@ int Table::Search
     return 1;
 }
 
+//This module accepts a key value, and searches the index file indexfile for the record where the date matches the specified value. IndexSearch then returns the block number key of the index record where the match occurs.
+
+        
 int Table::IndexSearch(string value)
 {
     //root, value;
