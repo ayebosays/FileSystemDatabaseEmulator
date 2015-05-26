01ddae4867a4336941e949ced52a2e4eb05c885d
SaundersB <speakeasys@gmail.com>
2015-05-26 09:54:58 -0700

    Adding definitions for Build_Table and Search.

--- CSE461Lab3/CSE461Lab3/Table.h
+++ CSE461Lab3/CSE461Lab3/Table.h
@@ -29,22 +29,63 @@ class Table: public Filesys
 
 Table::Table(string diskname, string flatfile, string indexfile)
 {
-    return 0;
+    return 1;
 }
 
 int Table::Build_Table
 {
-    return 0;
+    string block, record, date;
+    int blk_num;
+    vector<string> blocks, keys;
+    vector<int> links;
+    stringstream ss;
+    ifstream file(in_file.c_str());
+    
+    if (!file.good())
+    {
+        cout << "Was not able to build the table" << endl;
+        return -1;
+    }
+        
+    getline(file, record);
+    blk_num = fs -> addblock(flatfile, record);
+    
+        getline(file, record);
+        blk_num = fs -> addblock(flatfile, record);
+        
+        
+    
+    return 1;
 }
 
 int Table::Search
 {
-    return 0;
+    string buffer, date, end, place, type, ref, desc;
+    vector <string> record;
+    
+    int block_number = IndexSearch(value), lastPos, pos;
+    if(block_number == -1)
+    {
+        cout << "The record could not be found. " << endl;
+        return -1;
+    }
+    Filesys -> getblock(flatfile,block_number,buffer);
+    
+    
+    cout << "Record found: " << endl;
+    cout << "Date: " << record.at(0) << endl;
+    cout << "End: " << record.at(1) << endl;
+    cout << "Type: " << record.at(2) << endl;
+    cout << "Place: " << record.at(3) << endl;
+    cout << "Reference: " << record.at(4) << endl;
+    cout << "Description: " << record.at(5) <<< endl;
+    
+    return 1;
 }
 
 int Table::IndexSearch(string value)
 {
-    return 0;
+    return 1;
 }
 
 
